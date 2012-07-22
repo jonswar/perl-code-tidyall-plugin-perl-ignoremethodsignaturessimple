cfb54f1d808dda655bae9eb02d8254f16821e22c
Jonathan Swartz <swartz@pobox.com>
2012-07-22 08:20:45 -0700

    init

---
+++ lib/Code/TidyAll/Plugin/Perl/IgnoreMethodSignaturesSimple.pm
@@ -0,0 +1,42 @@
+package Code::TidyAll::Plugin::Perl::IgnoreMethodSignaturesSimple;
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Code::TidyAll::Plugin::Perl::IgnoreMethodSignaturesSimple - My great module
+
+=head1 SYNOPSIS
+
+    use Code::TidyAll::Plugin::Perl::IgnoreMethodSignaturesSimple;
+
+=head1 DESCRIPTION
+
+
+
+=head1 SUPPORT AND DOCUMENTATION
+
+Questions and feedback are welcome, and should be directed to the mailing list:
+
+    http://groups.google.com/...
+
+Bugs and feature requests will be tracked at RT:
+
+    http://rt.cpan.org/NoAuth/Bugs.html?Dist=Code-TidyAll-Plugin-Perl-IgnoreMethodSignaturesSimple
+    bug-code-tidyall-plugin-perl-ignoremethodsignaturessimple@rt.cpan.org
+
+The latest source code can be browsed and fetched at:
+
+    http://github.com/jonswar/perl-code-tidyall-plugin-perl-ignoremethodsignaturessimple
+    git clone git://github.com/jonswar/perl-code-tidyall-plugin-perl-ignoremethodsignaturessimple.git
+
+=head1 SEE ALSO
+
+L<Some::Module>
+
